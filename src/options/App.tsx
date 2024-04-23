7343b046e583eb47bc802749f79e5fd31251e059
asa <asa@example.com>
2024-04-24 07:22:18 +0900

    textareaのカーソルが後ろに移動され続けるバグを修正

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,26 +1,29 @@
 import { setChromeStorage } from '@storage/setChromeStorage'
 import { useChromeStorage } from '@storage/useChromeStorage'
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 import './style.scss'
+import { ChromeStorage } from '@models/ChromeStorage'
 
-interface Props {}
+interface Props {
+	extractStyleKey: ChromeStorage['extractStyleKey']
+	extractAttributes: ChromeStorage['extractAttributes']
+}
 
 const textareaNewLine = '\n'
 
-const styleKeyExample = ['font', 'background', 'color'].join('\n')
+const styleKeyExample = ['font-size', 'background', 'color'].join('\n')
 const attributesExample = ['id', 'style'].join('\n')
 
 export function App(props: Props) {
-	const {} = props
-	const { extractStyleKey, extractAttributes } = useChromeStorage()
-	const extractStyleKeyString = extractStyleKey.join(textareaNewLine)
-	const extractAttributesString = extractAttributes.join(textareaNewLine)
+	const [extractStyleKeyString, setExtractStyleKeyString] = useState(props.extractStyleKey.join(textareaNewLine))
+	const [extractAttributesString, setExtractAttributesString] = useState(props.extractAttributes.join(textareaNewLine))
 
 	return (
 		<>
 			<h1>codeタグから抽出するスタイルのキー(一行ずつ)</h1>
 			<div>例</div>
 			<textarea className='example' defaultValue={styleKeyExample}></textarea>
+			<aside>"font"は指定されていない可能性があるので非推奨です。代わりに"font-size"などを使ってください。</aside>
 			<textarea className='main' value={extractStyleKeyString} onChange={handleStyleKeyChange}></textarea>
 
 			<h1>codeタグから抽出する属性のキー(一行ずつ)</h1>
@@ -31,14 +34,16 @@ export function App(props: Props) {
 	)
 
 	function handleStyleKeyChange(event: ChangeEvent<HTMLTextAreaElement>) {
-		const keys = event.target.value.split(textareaNewLine).filter((e) => e !== '')
+		setExtractStyleKeyString(event.target.value)
 
+		const keys = event.target.value.split(textareaNewLine).filter((e) => e !== '')
 		setChromeStorage('extractStyleKey', keys)
 	}
 
 	function handleAttributesChange(event: ChangeEvent<HTMLTextAreaElement>) {
-		const attributes = event.target.value.split(textareaNewLine).filter((e) => e !== '')
+		setExtractAttributesString(event.target.value)
 
+		const attributes = event.target.value.split(textareaNewLine).filter((e) => e !== '')
 		setChromeStorage('extractAttributes', attributes)
 	}
 }
