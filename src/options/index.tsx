bbebfd6d28a439b4bdf50b9321026a5c8b0a3d41
asa <asa@example.com>
2024-04-24 07:22:38 +0900

    textareaの初期値を渡すように修正

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -1,8 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { getChromeStorage } from '@storage/getChromeStorage'
 
 const root = document.getElementById('root')
 
 if (root) {
-	createRoot(root).render(<App />)
+	getChromeStorage().then((storage) => {
+		const { extractAttributes, extractStyleKey } = storage
+		createRoot(root).render(<App extractAttributes={extractAttributes} extractStyleKey={extractStyleKey} />)
+	})
 }
