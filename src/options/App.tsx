e00aac89b0d546550d0a87055f264e3014d9bba9
asa <asa@example.com>
2024-04-24 09:05:39 +0900

    タイプのたびに変更を保存しないよう修正

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,8 +1,8 @@
-import { setChromeStorage } from '@storage/setChromeStorage'
+import { setBulkChromeStorage, setChromeStorage } from '@storage/setChromeStorage'
 import { useChromeStorage } from '@storage/useChromeStorage'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, TextareaHTMLAttributes, useState } from 'react'
 import './style.scss'
-import { ChromeStorage } from '@models/ChromeStorage'
+import { ChromeStorage, ChromeStorageKey } from '@models/ChromeStorage'
 
 interface Props {
 	extractStyleKey: ChromeStorage['extractStyleKey']
@@ -24,26 +24,40 @@ export function App(props: Props) {
 			<div>例</div>
 			<textarea className='example' defaultValue={styleKeyExample}></textarea>
 			<aside>"font"は指定されていない可能性があるので非推奨です。代わりに"font-size"などを使ってください。</aside>
-			<textarea className='main' value={extractStyleKeyString} onChange={handleStyleKeyChange}></textarea>
+			<textarea className='main' value={extractStyleKeyString} onChange={handleStyleKeyChange} />
+			<button onClick={handleExtractStyleKeySaveClick}>保存</button>
 
 			<h1>codeタグから抽出する属性のキー(一行ずつ)</h1>
 			<div>例</div>
 			<textarea className='example' defaultValue={attributesExample}></textarea>
-			<textarea className='main' value={extractAttributesString} onChange={handleAttributesChange}></textarea>
+			<textarea className='main' value={extractAttributesString} onChange={handleAttributesChange} />
+			<button onClick={handleExtractAttributesSaveClick}>保存</button>
 		</>
 	)
 
+	function handleExtractStyleKeySaveClick() {
+		const keys = extractStyleKeyString.split(textareaNewLine).filter((e) => e !== '')
+		setChromeStorage('extractStyleKey', keys)
+	}
+
+	function handleExtractAttributesSaveClick() {
+		const attributes = extractAttributesString.split(textareaNewLine).filter((e) => e !== '')
+		setChromeStorage('extractAttributes', attributes)
+	}
+
 	function handleStyleKeyChange(event: ChangeEvent<HTMLTextAreaElement>) {
 		setExtractStyleKeyString(event.target.value)
-
-		const keys = event.target.value.split(textareaNewLine).filter((e) => e !== '')
-		setChromeStorage('extractStyleKey', keys)
 	}
 
 	function handleAttributesChange(event: ChangeEvent<HTMLTextAreaElement>) {
 		setExtractAttributesString(event.target.value)
-
-		const attributes = event.target.value.split(textareaNewLine).filter((e) => e !== '')
-		setChromeStorage('extractAttributes', attributes)
 	}
+}
+
+interface NamedTextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+	nameAttributeValue: ChromeStorageKey
+}
+
+function NamedTextarea({ nameAttributeValue, ...otherProps }: NamedTextareaProps) {
+	return <textarea name={nameAttributeValue} {...otherProps} />
 }
