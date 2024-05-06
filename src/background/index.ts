5f2c18764162270ed9dcd4ef9f538431f6d838e0
asa <asa@example.com>
2024-05-07 06:49:55 +0900

    execReplaceの値でアイコンを変える

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,10 +1,35 @@
+import { ChromeStorageKey } from '@models/ChromeStorage'
 import { message } from '@root/message/message'
+import { getChromeStorage } from '@storage/getChromeStorage'
+import { isBoolean } from '@utils/isBoolean'
 
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
 	const { tabId } = details
 	chrome.tabs.sendMessage(tabId, message.historyChanged).catch()
 })
 
-chrome.action.setIcon({
-	path: '../icon16checked.png',
+getChromeStorage().then(({ execReplace }) => {
+	changeIconByExecReplace(execReplace)
+})
+
+async function changeIconByExecReplace(execReplace: boolean) {
+	const iconPath = execReplace ? '../icon16checked.png' : '../icon16.png'
+
+	chrome.action.setIcon({
+		path: iconPath,
+	})
+}
+
+chrome.storage.local.onChanged.addListener((changes) => {
+	const targetKey: ChromeStorageKey = 'execReplace'
+
+	for (const [storageKey, storageChange] of Object.entries(changes)) {
+		if (storageKey === targetKey) {
+			const value = storageChange.newValue
+
+			if (isBoolean(value)) {
+				changeIconByExecReplace(storageChange.newValue)
+			}
+		}
+	}
 })
